3271f40ce51e6d5e10cc6c4abe3213efe29839b6
bilalEBR <bilal999ebrahim@gmail.com>
2026-05-04 20:38:24 +0300

    first commit

---
+++ src/main/java/com/online_booking/online_booking_reservation/OnlineBookingReservationApplication.java
@@ -0,0 +1,13 @@
+package com.online_booking.online_booking_reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBookingReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBookingReservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/online_booking/online_booking_reservation/OnlineBookingReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.online_booking.online_booking_reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBookingReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
